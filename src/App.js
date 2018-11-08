2f21b1a706bba40e2d86cbd763a1a36564fa3f12
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-11-08 11:20:15 +0100

    React3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Quotes from "./Quotes";
+import Lamp from "./Lamp"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        work: props.work
+      };
+    }
+    handleClick = () => {
+      this.setState({ work: !this.state.work });
+    };
   render() {
+    const working = this.state.work ? 'one' : 'App-logo';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" className={working} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <button onClick={this.handleClick}>Work</button>
+        <Lamp on />
+        <Lamp />
+        <Quotes />
       </div>
     );
   }
